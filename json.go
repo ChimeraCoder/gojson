561f68c1ed4c1584213608e8618b2f4282cb126d
aditya <dev@chimeracoder.net>
2012-12-24 15:05:53 -0500

    Add command-line argument parsing

--- json.go
+++ json.go
@@ -2,30 +2,28 @@ package main
 
 import (
     "encoding/json"
-    "log"
+    "flag"
+    "fmt"
+    "io/ioutil"
     "reflect"
+    "os"
 )
 
 
 func generateTypes(obj map[string]interface{}, layers int) string {
-    //structure := make(map[string]string)
     structure := "struct {"
 
     for key, _ := range obj{
         curType:= reflect.TypeOf(obj[key])
-        log.Printf("Type is %v", curType)
         indentation := "\t"
         for i := 0; i < layers;{
             indentation += "\t"
             i++
         }
 
-        //Check if this is a nested json object
-        log.Printf("Object %v", obj[key])
         nested, isNested:= obj[key].(map[string]interface{})
         if isNested {
             //This is a nested object
-            //structure[key] = generateTypes(nested)
             structure += "\n" + indentation + key + " " + generateTypes(nested, layers + 1) + "}"
         } else{
             //Check if this is an array
@@ -37,14 +35,14 @@ func generateTypes(obj map[string]interface{}, layers int) string {
             } else {
                 structure += "\n" + indentation + key + " " + curType.Name() 
             }
-
-            //structure[key] = curType.Name()
         }
     }
-    //return prettyPrint(structure)
     return structure
 }
 
+
+//Given a JSON string representation of an object and a name structName,
+//generate the struct definition of the struct, and give it the specified name
 func generate(jsn string, structName string) string{
     bts := []byte(jsn)
     result := map[string]interface{}{}
@@ -56,30 +54,18 @@ func generate(jsn string, structName string) string{
 }
 
 
+var filename *string =  flag.String("file", os.Args[0], "the name of the file that contains the json")
 
 func main(){
-
-    json := `{
-    "glossary": {
-        "title": "example glossary",
-        "GlossDiv": {
-            "title": "S",
-            "GlossList": {
-                "GlossEntry": {
-                    "ID": "SGML",
-                    "SortAs": "SGML",
-                    "GlossTerm": "Standard Generalized Markup Language",
-                    "Acronym": "SGML",
-                    "Abbrev": "ISO 8879:1986",
-                    "GlossDef": {
-                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                        "GlossSeeAlso": ["GML", "XML"]
-                    },
-                    "GlossSee": "markup"
-                }
-            }
-        }
+    flag.Parse()
+    if *filename == "" {
+        panic(fmt.Errorf("No filename specified"))
     }
-}`
-    log.Printf("Result: \n%v", generate(json, "TestStruct"))
+
+    //Demontrate example
+    //using http://json.org/example.html
+
+    js, _  := ioutil.ReadFile(*filename)
+
+    fmt.Printf("package main\n%v\n", generate(string(js), "TestStruct"))
 }
