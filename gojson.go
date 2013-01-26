a0c54f0fbfaacefd486e8f8de51b64a1bf7040ca
aditya <dev@chimeracoder.net>
2013-01-26 17:59:28 -0500

    Automatically generate properly-formatted output accoridng to go fmt

--- gojson.go
+++ gojson.go
@@ -1,70 +1,82 @@
 package main
 
 import (
-    "encoding/json"
-    "flag"
-    "fmt"
-    "io/ioutil"
-    "reflect"
-    "os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"io/ioutil"
+	"os"
+	"reflect"
 )
 
-
 func generateTypes(obj map[string]interface{}, layers int) string {
-    structure := "struct {"
+	structure := "struct {"
 
-    for key, _ := range obj{
-        curType:= reflect.TypeOf(obj[key])
-        indentation := "\t"
-        for i := 0; i < layers;{
-            indentation += "\t"
-            i++
-        }
+	for key, _ := range obj {
+		curType := reflect.TypeOf(obj[key])
+		indentation := "\t"
+		for i := 0; i < layers; {
+			indentation += "\t"
+			i++
+		}
 
-        nested, isNested:= obj[key].(map[string]interface{})
-        if isNested {
-            //This is a nested object
-            structure += "\n" + indentation + key + " " + generateTypes(nested, layers + 1) + "}"
-        } else{
-            //Check if this is an array
-            _, isArray := obj[key].([]interface{})
-            if isArray {
-                //Currently defaults to interface{} because JSON allows for heterogeneous arrays
-                //TODO Run type inference on array to see if it is an array of a single type
-                structure += "\n" + indentation + key + " []interface{}"
-            } else {
-                structure += "\n" + indentation + key + " " + curType.Name()
-            }
-        }
-    }
-    return structure
+		nested, isNested := obj[key].(map[string]interface{})
+		if isNested {
+			//This is a nested object
+			structure += "\n" + indentation + key + " " + generateTypes(nested, layers+1) + "}"
+		} else {
+			//Check if this is an array
+			_, isArray := obj[key].([]interface{})
+			if isArray {
+				//Currently defaults to interface{} because JSON allows for heterogeneous arrays
+				//TODO Run type inference on array to see if it is an array of a single type
+				structure += "\n" + indentation + key + " []interface{}"
+			} else {
+				structure += "\n" + indentation + key + " " + curType.Name()
+			}
+		}
+	}
+	return structure
 }
-
 
 //Given a JSON string representation of an object and a name structName,
 //generate the struct definition of the struct, and give it the specified name
-func generate(jsn []byte, structName string) string{
-    result := map[string]interface{}{}
-    json.Unmarshal(jsn, &result)
+func generate(jsn []byte, structName string) (err error) {
+	result := map[string]interface{}{}
+	json.Unmarshal(jsn, &result)
 
-    typeString := generateTypes(result, 0)
+	typeString := generateTypes(result, 0)
 
-    return "type " + structName + " " + typeString + "}"
+	typeString = "package main \n type " + structName + " " + typeString + "}"
+
+	fset := token.NewFileSet() // positions are relative to fset
+
+	formatted, err := parser.ParseFile(fset, "", typeString, parser.ParseComments)
+	if err != nil {
+		return err
+	}
+
+	printer.Fprint(os.Stdout, fset, formatted)
+	return
 }
 
+var input_file *string = flag.String("file", os.Args[1], "the name of the file that contains the json")
 
-var filename *string =  flag.String("file", os.Args[1], "the name of the file that contains the json")
+func main() {
+	flag.Parse()
+	if *input_file == "" {
+		panic(fmt.Errorf("No input file specified"))
+	}
 
-func main(){
-    flag.Parse()
-    if *filename == "" {
-        panic(fmt.Errorf("No filename specified"))
-    }
+	//Demontrate example
+	//using http://json.org/example.html
 
-    //Demontrate example
-    //using http://json.org/example.html
+	js, _ := ioutil.ReadFile(*input_file)
 
-    js, _  := ioutil.ReadFile(*filename)
-
-    fmt.Printf("package main\n%v\n", generate(js, "TestStruct"))
+	if err := generate(js, "TestStruct"); err != nil {
+		panic(err)
+	}
 }
