45ec9dac1968e61aef8d33b70523d3bdd4026295
Trevor Rothaus <trevor.rothaus@sendgrid.com>
2015-06-10 17:58:57 -0700

    optional package name

--- json-to-struct.go
+++ json-to-struct.go
@@ -148,6 +148,10 @@ var commonInitialisms = map[string]bool{
 // Given a JSON string representation of an object and a name structName,
 // attemp to generate a struct definition
 func Generate(input io.Reader, structName, pkgName string) ([]byte, error) {
+	pkg := ""
+	if pkgName != "" {
+		pkg = fmt.Sprintf("package %s\n", pkgName)
+	}
 	var iresult interface{}
 	var result map[string]interface{}
 	if err := json.NewDecoder(input).Decode(&iresult); err != nil {
@@ -164,8 +168,8 @@ func Generate(input io.Reader, structName, pkgName string) ([]byte, error) {
 			return nil, fmt.Errorf("empty array")
 		}
 	case []interface{}:
-		src := fmt.Sprintf("package %s\n\ntype %s %s\n",
-			pkgName,
+		src := fmt.Sprintf("%s\ntype %s %s\n",
+			pkg,
 			structName,
 			"[]interface{}")
 		return []byte(src), nil
@@ -174,8 +178,8 @@ func Generate(input io.Reader, structName, pkgName string) ([]byte, error) {
 		return nil, fmt.Errorf("unexpected type: %T", iresult)
 	}
 
-	src := fmt.Sprintf("package %s\ntype %s %s}",
-		pkgName,
+	src := fmt.Sprintf("%stype %s %s}",
+		pkg,
 		structName,
 		generateTypes(result, 0))
 	formatted, err := format.Source([]byte(src))
