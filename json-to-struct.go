b367c382df32de1d76dc748d8b1002dfce09ace1
Parshwa Shah <pshah@yext.com>
2019-06-06 15:52:38 -0400

    fix interface slice scenario
    When the JSON is of format [interface,interface,interface] gojson has trouble printing out the substructs.

--- json-to-struct.go
+++ json-to-struct.go
@@ -217,15 +217,9 @@ func Generate(input io.Reader, parser Parser, structName, pkgName string, tags [
 	case map[string]interface{}:
 		result = iresult
 	case []interface{}:
-		src := fmt.Sprintf("package %s\n\ntype %s %s\n",
-			pkgName,
-			structName,
-			typeForValue(iresult, structName, tags, subStructMap, convertFloats))
-		formatted, err := format.Source([]byte(src))
-		if err != nil {
-			err = fmt.Errorf("error formatting: %s, was formatting\n%s", err, src)
-		}
-		return formatted, err
+		 result = map[string]interface{}{
+	        	"temp" : iresult,
+        	}
 	default:
 		return nil, fmt.Errorf("unexpected type: %T", iresult)
 	}
